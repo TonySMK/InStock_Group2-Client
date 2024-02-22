7b7b55f8c4d80358509d0ca1bdecd27bd37d3162
Wanessa Lopes <wanessaalopes@gmail.com>
2024-02-22 14:26:49 -0700

    Delete not useful lines in App.js

--- src/App.js
+++ src/App.js
@@ -26,20 +26,6 @@ function App() {
           <Route path="warehouses/add" element={<WarehousesAddItem/>}></Route>
         </Routes>
       </BrowserRouter>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }

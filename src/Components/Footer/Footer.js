44aa089713fbcc33ddbf21385b3699a33d234b5e
Ryan Park <ryan9611@hotmail.com>
2024-02-24 15:45:38 -0500

    Adjusted Footer text and alignment

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer () {
     return (
         <>
             <footer className = 'footer'>
-                <p className = 'footer__text'>&copy; BrainStation Inc. All Rights Reserved.</p>
+                <p className = 'footer__text'>&copy; InStock Inc. All Rights Reserved.</p>
             </footer>        
         </>
     )

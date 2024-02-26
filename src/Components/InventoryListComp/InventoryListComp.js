5c7d96b2065ba38d52dc9c2cacdc0211ece10571
TonySMK <tonysun25light@live.ca>
2024-02-26 18:05:27 -0500

    Implemented mapping of json from backend

--- src/Components/InventoryListComp/InventoryListComp.js
+++ src/Components/InventoryListComp/InventoryListComp.js
@@ -1,37 +1,69 @@
 import "./InventoryListStyles.scss";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function InventoryListComp({ object }) {
+  const [compstate, setCompState] = useState(true);
+  const [invenstoryList, setInventoryList] = useState("");
+  const [renderedList, setRenderedList] = useState("");
   // {object} is the array of objects that the this component will map
-  // and render out
+  // and render outbottom__right
 
   // remember to stop propigation for the delete and edit buttons
+  function fetch() {
+    axios.get("http://localhost:8080/api/inventories").then((res) => {
+      console.log(res.data);
+      setInventoryList(res.data);
+      renderList();
+    });
+  }
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  function renderList() {
+    // let somelist = theList;
+    const therender = invenstoryList.map((row) => (
+      <section className="row" key={row.id.toString()}>
+        <div className="row__left">
+          <button className="row__left__item">{row.item_name}</button>
+          <div className="row__left__category">{row.category}</div>
+        </div>
+
+        <div className="row__middle">
+          <div className="row__middle__status">{row.status}</div>
+          <div className="row__middle__quantity">{row.quantity}</div>
+          <div className="row__middle__warehouselocation">
+            {row.warehouse_name}
+          </div>
+        </div>
+
+        <div className="row__bottom">
+          <button className="row__bottom__deleteitem">delete button</button>
+          <button className="row__bottom__edititem">edit button</button>
+        </div>
+      </section>
+    ));
+    setCompState(false);
+    setRenderedList(therender);
+  }
+
   return (
     <>
-      <div classname="parentwrapper">
-        <div classname="top">
-          <div classname="title"></div>
-          <div classname="searchbar"></div>
-          <div classname="addinventory"></div>
+      {compstate ? (
+        <section>Loading Data...</section>
+      ) : (
+        <div className="parentwrapper">
+          <div className="top">
+            <div className="top__title"></div>
+            <div className="top__searchbar"></div>
+            <div className="top__addinventory"></div>
+          </div>
+
+          <div id="bottomlist">{renderedList}</div>
         </div>
-
-        <div classname="bottom">
-          <button onclick>
-            <div classname="bottom__left">
-              <div classname="bottom__left__item"></div>
-              <div classname="bottom__left__category"></div>
-            </div>
-
-            <div classname="bottom__right">
-              <div classname="bottom__right__status"></div>
-              <div classname="bottom__right__quantity"></div>
-              <div classname="bottom__right__warehouselocation"></div>
-            </div>
-
-            <button classname="deleteitem"></button>
-            <button classname="edititem"></button>
-          </button>
-        </div>
-      </div>
+      )}
     </>
   );
 }

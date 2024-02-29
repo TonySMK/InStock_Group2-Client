8dcbd4430464250415f5da6944f65bad7cdd52cb
TonySMK <tonysun25light@live.ca>
2024-02-28 22:11:59 -0500

    update...

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import WarehousesAddItem from "./Pages/WarehousesAddItem/WarehousesAddItem";
 import WarehousesDetails from "./Pages/WarehousesDetails/WarehousesDetails";
 import WarehousesEditItem from "./Pages/WarehousesEditItem/WarehousesEditItem";
 import Header from "./Components/Header/Header";
-
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div className="App">
@@ -38,6 +38,7 @@ function App() {
           </Routes>
         </BrowserRouter>
       </section>
+      <Footer />
     </div>
   );
 }

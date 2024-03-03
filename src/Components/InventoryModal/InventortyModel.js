7fc82d829e3a06fb85cbaf8cdd0184744b46cc2d
TonySMK <tonysun25light@live.ca>
2024-03-02 20:24:36 -0500

    Implemented 90% styling, adjusted styling for... inventory page and inventory list comp

--- src/Components/InventoryModal/InventortyModel.js
+++ src/Components/InventoryModal/InventortyModel.js
@@ -8,34 +8,56 @@ function InventoryModel({
   deleteButtonHandler,
   targetinformtion,
 }) {
+  let thebodyselector = document.getElementById("root");
   if (!isModelOpen) {
+    thebodyselector.style.overflowY = "scoll";
+    thebodyselector.style.height = "auto";
     return null;
+  } else {
+    thebodyselector.style.overflowY = "hidden";
+    thebodyselector.style.height = "100vh";
+  }
+
+  function finaldeletehandler(theTargert) {
+    deleteButtonHandler(theTargert);
+    closeModalHandler();
   }
 
   return createPortal(
     <>
       <div className="overlay"></div>
       <div className="modalbody">
-        <button className="modalbody__closebuttonwrapper">
-          <img
-            className="modalbody__closebuttonwrapper--imagecloseicon"
-            onClick={closeModalHandler}
-            src={closeIcon}
-            alt="Delete Icon Button"
-          />
-        </button>
-
-        <div className="modalbody__questionprompt">{`Delete ${targetinformtion[1]} inventory item?`}</div>
-        <div className="modalbody__warningmessage">
-          Please confirm that you’d like to delete Television from the inventory
-          list. You won’t be able to undo this action.
+        <div className="modalbody__upperpanel">
+          <button className="modalbody__upperpanel__closebuttonwrapper">
+            <img
+              className="closebuttonimagecloseicon"
+              onClick={closeModalHandler}
+              src={closeIcon}
+              alt="Delete Icon Button"
+            />
+          </button>
+          <h2 className="modalbody__upperpanel__questionprompt">{`Delete ${targetinformtion[1]} inventory item?`}</h2>
+          <div className="modalbody__upperpanel__warningmessage">
+            {`Please confirm that you’d like to delete ${targetinformtion[1]} from the inventory
+        list. You won’t be able to undo this action.`}
+          </div>
         </div>
 
         <section className="modalbody__actionbuttonswrapper">
-          <button className="modalbody__actionbuttonswrapper--canel">
+          <button
+            className="modalbody__actionbuttonswrapper__canel"
+            onClick={() => {
+              closeModalHandler();
+            }}
+          >
             Cancel
           </button>
-          <button className="modalbody__actionbuttonswrapper--delete">
+          <button
+            className="modalbody__actionbuttonswrapper__delete"
+            onClick={() => {
+              finaldeletehandler(targetinformtion[0]);
+            }}
+          >
             Delete
           </button>
         </section>

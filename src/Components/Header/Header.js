1617d4ae3286b0e7dc4a048171b8c3e0bb1c546e
Lucille Alvar <lucille.alvar@gmail.com>
2024-03-03 13:44:49 -0700

    Linked the header component to it's right pages. Clicking warehouse will direct to warehouses pages and clicking inventory will direct to home page/inventory page.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,19 @@
+import React from "react";
 import "./Header.scss";
 import InStockLogo from "../../Assets/Logo/InStock-Logo_2x.png";
 
 function Header() {
+const navigateTo = (path) => {
+  window.location.href = path;
+}
   return (
     <>
       <section className="header">
         <div className="header__container">
           <img className="header__logo" src={InStockLogo}></img>
           <div className="header__button-container">
-            <button className="header__nav-warehouses">Warehouses</button>
-            <button className="header__nav-inventory">Inventory</button>
+            <button className="header__nav-warehouses" onClick={() => navigateTo("/warehouses")}>Warehouses</button>
+            <button className="header__nav-inventory" onClick={() => navigateTo("/")}>Inventory</button>
           </div>
         </div>
       </section>
